fdc42ad06ba11fc80e5120c36daa3cf4ac4c1449
hoshianaaa <hoshina12345@gmail.com>
2022-01-16 12:37:43 +0900

    ros node 化

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <ros/ros.h> // 追加
+
 
 int main(int argc, char *argv[])
 {
+
+    ros::init(argc, argv, "geometric_match_ros"); // 追加
+
+    ros::NodeHandle nh = ros::NodeHandle(); // 追加
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
     return a.exec();
 }
+
