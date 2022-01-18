9fd6e2c991026cc2cd2ec9e8f6eed2e7ecde5fed
hoshianaaa <hoshina12345@gmail.com>
2022-01-18 14:05:59 +0900

    マッチ処理のループ化

--- algorithm/geomatch.h
+++ algorithm/geomatch.h
@@ -299,7 +299,7 @@ int func(Mat template_img, Mat search_img, int temp_canny_low, int temp_canny_hi
     imshow("w frame2", wframe2);
 //    imshow("simg", simg);
     imwrite("w_frame.png", wframe);
-    waitKey();
+    waitKey(1);
     return 1;
 }
 
